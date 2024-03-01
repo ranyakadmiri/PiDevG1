19b7fa65199499a2cca98d92f18d0dadee12fef3
mouna30 <mouna.jomaa@esprit.tn>
2024-03-01 16:40:51 +0100

    version final

---
+++ WeBuild/src/Controller/SearchController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchController extends AbstractController
+{
+    #[Route('/search', name: 'app_search')]
+    public function index(): Response
+    {
+        return $this->render('search/index.html.twig', [
+            'controller_name' => 'SearchController',
+        ]);
+    }
+
+    
+}
