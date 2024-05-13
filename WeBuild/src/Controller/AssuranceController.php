a90a9cad5d61ad88ffc1e5175bdd04310e7ecf6c
ranya <ranya.azizikadmiri@esprit.tn>
2024-05-13 18:56:27 +0100

    upload image assurance withou display

--- WeBuild/src/Controller/AssuranceController.php
+++ WeBuild/src/Controller/AssuranceController.php
@@ -11,6 +11,10 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
 
 class AssuranceController extends AbstractController
 {
@@ -34,20 +38,50 @@ class AssuranceController extends AbstractController
 
 
     #[Route('/assurance/add', name: 'app_add')]
-    public function Add(ManagerRegistry $d, Request $request): Response
+
+    #[Route('/assurance/add', name: 'app_add')]
+    public function add(ManagerRegistry $d, Request $request, SluggerInterface $slugger): Response
     {
         $Assurance = new Assurance();
         $em = $d->getManager();
         $form = $this->createForm(AssuranceType::class, $Assurance);
         $form->add('ajouter', SubmitType::class);
         $form->handleRequest($request);
-        if ($form->isSubmitted()) {
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Handle image upload
+            $imageFile = $form->get('image')->getData();
 
+            // If an image was uploaded
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // This is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+
+                // Move the file to the directory where images are stored
+                try {
+                    // Specify the full path including the filename
+                    $imageFile->move(
+                        'C:/xampp/htdocs/images',
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // Handle file upload error
+                }
+
+                // Update the 'imageUrl' property to store the full path
+                $fullPath = 'C:/xampp/htdocs/images/' . $newFilename;
+                $Assurance->setImage($fullPath);
+            }
+
+            // Persist the data
             $em->persist($Assurance);
             $em->flush();
+
             return $this->redirectToRoute('app_show');
         }
+
         return $this->render('assurance/add.html.twig', ["form" => $form->createView()]);
     }
 
--- WeBuild/src/Form/AssuranceType.php
+++ WeBuild/src/Form/AssuranceType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class AssuranceType extends AbstractType
 {
@@ -18,7 +19,13 @@ class AssuranceType extends AbstractType
             ["class" => "form-control"]])
             ->add('description', TextareaType::class, ["attr" =>
             ["class" => "form-control"]])
-            ->add('image', TextType::class)
+            // Add image field to your form type
+
+            ->add('image', FileType::class, [
+                'label' => 'Image (JPEG, PNG)',
+                'mapped' => false, // Tell Symfony not to try to map this field to any entity property
+                'required' => false, // This field is not required
+            ])
             ->add('condition_age', TextType::class, ["attr" =>
             ["class" => "form-control"]])
             ->add('condition_medicale', TextType::class, ["attr" =>
